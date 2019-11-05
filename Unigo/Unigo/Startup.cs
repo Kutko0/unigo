3c02c11ffa96c43ba9cea8c70456b4264e50d0c5
Kutko0 <kutko145@gmail.com>
2019-11-05 09:56:42 +0100

    Init commit

---
+++ Unigo/Unigo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Unigo.Startup))]
+namespace Unigo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
