f60b29405293e88c2dc8d81cb33a9e59e8ea6323
Kutko0 <kutko145@gmail.com>
2019-11-07 09:55:06 +0100

    Added some DB migrations

---
+++ Unigo/Unigo/Migrations/201911061039315_PopulatingDestination.cs
@@ -0,0 +1,22 @@
+﻿namespace Unigo.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PopulatingDestination : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("INSERT INTO Destinations( Name) VALUES('Hobrovej 85')");
+            Sql("INSERT INTO Destinations( Name) VALUES('Sofiendalsvej 60')");
+            Sql("INSERT INTO Destinations( Name) VALUES('Lerpyttervej 43 (Thisted)')");
+            Sql("INSERT INTO Destinations( Name) VALUES('Mylius Erichsens Vej 137')");
+            Sql("INSERT INTO Destinations( Name) VALUES('Selma Lagerløfs Vej 2')");
+            Sql("INSERT INTO Destinations( Name) VALUES('Skolevangen 45 (Hjørring)')");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
