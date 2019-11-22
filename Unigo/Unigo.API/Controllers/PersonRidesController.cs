c8ebc3d446d6916cb8024edad8b4dcf88db9f2ae
Denis Raslavsky <denis.raslavsky@gmail.com>
2019-11-22 17:09:13 +0100

    StopPointRide, PersonRide API

---
+++ Unigo/Unigo.API/Controllers/PersonRidesController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Unigo.Data;
+using Unigo.Repo;
+
+namespace Unigo.API.Controllers
+{
+    [RoutePrefix("api/personRides")]
+    public class PersonRidesController : ApiController
+    {
+        public IRepository<PersonRide> personRidesRepository;
+
+        public PersonRidesController(IRepository<PersonRide> personRidesRepo)
+        {
+            this.personRidesRepository = personRidesRepo;
+        }
+
+        [HttpGet]
+        
+        public IList<PersonRide> GetAllPersonRide()
+        {
+            return personRidesRepository.GetAll().ToList();
+        }
+
+        [HttpGet]
+        [Route("ByRideId/{id}")]
+        public IHttpActionResult GetByRideId(int id)
+        {
+            IList<PersonRide> personRides = personRidesRepository.GetAll().ToList();
+            var chosenPersonRides = from personRide in personRides
+                                    where personRide.RideId == id
+                                    select personRide;
+
+            return Ok(chosenPersonRides);
+        }
+
+        [HttpGet]
+        [Route("ByPersonId/{id}")]
+        public IHttpActionResult GetByPersonId(int id)
+        {
+            IList<PersonRide> personRides = personRidesRepository.GetAll().ToList();
+            var chosenPersonRides = from personRide in personRides
+                                    where personRide.PersonId == id
+                                    select personRide;
+
+            return Ok(chosenPersonRides);
+        }
+
+        [HttpPost]
+       public IHttpActionResult CreatePersonRide(PersonRide personRide)
+        {
+            //Checks data annotations in model class PersonRide
+            if (!ModelState.IsValid)
+                return BadRequest("Not a valid model");
+
+            personRidesRepository.Add(personRide);
+            personRidesRepository.SaveChanges();
+
+            return Created(new Uri(Request.RequestUri + "/" + personRide.Id), personRide);
+        }
+
+        [HttpDelete]
+        public void RemovePersonRide(int id)
+        {
+            if (personRidesRepository.GetById(id) == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            personRidesRepository.RemoveById(id);
+            personRidesRepository.SaveChanges();
+        }
+    }
+}
---
+++ Unigo/Unigo.API/Controllers/StopPointRidesController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Unigo.Data;
+using Unigo.Repo;
+
+namespace Unigo.API.Controllers
+{
+    public class StopPointRidesController : ApiController
+    {
+        public IRepository<StopPointRide> stopPointRidesRepository;
+
+        public StopPointRidesController(IRepository<StopPointRide> stopPointRidesRepo)
+        {
+            this.stopPointRidesRepository = stopPointRidesRepo;
+        }
+
+        [HttpGet]
+        public IList<StopPointRide> GetAll()
+        {
+            return stopPointRidesRepository.GetAll().ToList();
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetByRideId(int id)
+        {
+            IList<StopPointRide> stopPointRides = stopPointRidesRepository.GetAll().ToList();
+            var chosenStopPointRides = from stopPointRide in stopPointRides
+                                       where stopPointRide.RideId == id
+                                       select stopPointRide;
+
+            return Ok(chosenStopPointRides);
+        }
+
+        [HttpPost]
+        public IHttpActionResult CreateStopPointRide(StopPointRide stopPointRide)
+        {
+            //Checks data annotations in model class StopPointRide
+            if (!ModelState.IsValid)
+                return BadRequest("Not a valid model");
+
+            stopPointRidesRepository.Add(stopPointRide);
+            stopPointRidesRepository.SaveChanges();
+
+            return Created(new Uri(Request.RequestUri + "/" + stopPointRide.Id), stopPointRide);
+        } 
+
+        [HttpPut]
+        public IHttpActionResult UpdateStopPointRide(int id, StopPointRide stopPointRide)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("Not a valid model");
+
+
+            var existingStopPointRide = stopPointRidesRepository.GetById(id);
+
+            if (existingStopPointRide != null)
+            {
+                existingStopPointRide.Location = stopPointRide.Location;
+                existingStopPointRide.LeavingTime = stopPointRide.LeavingTime;
+
+                stopPointRidesRepository.SaveChanges();
+            }
+            else
+            {
+                return NotFound();
+            }
+
+            return Ok();
+        }
+
+        [HttpDelete]
+        public void RemoveStopPointRide(int id)
+        {
+            if (stopPointRidesRepository.GetById(id) == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            stopPointRidesRepository.RemoveById(id);
+            stopPointRidesRepository.SaveChanges();
+        }
+
+
+    }
+}
